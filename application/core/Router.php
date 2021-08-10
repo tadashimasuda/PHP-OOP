eee82e73c1f4e9e17c45ca7aa7c2cdd20695d240
tadashimasuda <tmasu4260@gmail.com>
2021-08-10 15:12:13 +0900

    update Router.php

--- application/core/Router.php
+++ application/core/Router.php
@@ -34,7 +34,7 @@ class Router{
 
         foreach ($this->routes as $pattern => $params) {
             if (preg_match('#^'.$pattern.'$#',$path_info,$matches)){
-                $params = array_merge($params,$matches);8
+                $params = array_merge($params,$matches);
 
                 return $params;
             }
