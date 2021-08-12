f534dfd2a75d8386aa3827bdf29191c35be1531c
tadashimasuda <tmasu4260@gmail.com>
2021-08-12 19:10:30 +0900

    Controller classにリダイレクトの追加

--- application/core/Controller.php
+++ application/core/Controller.php
@@ -3,21 +3,21 @@
 abstract class Controller{
     protected $controller_name;
     protected $action_name;
-    protected $application;
+    protected $appllication;
     protected $request;
     protected $response;
     protected $session;
     protected $db_manager;
 
-    public function __construct()
+    public function __construct($appllication)
     {
         $this->controller_name = strtolower(substr(get_class($this),0,-10));
 
-        $this->application = $application;
-        $this->request = $application->getRequest();
-        $this->response = $application->getResponse();
-        $this->session = $application->getSession();
-        $this->db_manager = $application->getDbManager();
+        $this->appllication = $appllication;
+        $this->request = $appllication->getRequest();
+        $this->response = $appllication->getResponse();
+        $this->session = $appllication->getSession();
+        $this->db_manager = $appllication->getDbManager();
     }
 
     public function run($action,$params = array()){
@@ -50,6 +50,24 @@ abstract class Controller{
         $path = $this->controller_name.'/'.$template;
 
         return $view->render($path,$variables,$layout);
+
+    }
+
+    protected function forward404(){
+        throw new HttpNotFoundException('Found 404 page form '.$this->controller_name.'/'.$this->action_name);
+    }
+
+    protected function redirect($url){
+        if (!preg_match('#https?://#',$url)){
+            $protocol = $this->request->isSsl() ? 'https://' : 'http://';
+            $host = $this->request->getHost();
+            $base_url = $this->request->getBaseUrl();
+
+            $url = $protocol . $host . $base_url . $url;
+        }
+
+        $this->response->setStatusCode(302,'Found');
+        $this->response->setStatusCode('Location',$url);
 
     }
 }
